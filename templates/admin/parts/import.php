55fc7c2ac8ef2bd6ace9918e028336ec4ef6eaf5
honorcoding <john@honorcoding.com>
2024-05-14 10:28:07 -0800

    update

--- templates/admin/parts/import.php
+++ templates/admin/parts/import.php
@@ -10,6 +10,8 @@ $example_csv_url = ITW_MEDICAL_PRODUCTS_URL . 'assets/example.csv';
         <li>If the "image" field is left blank, then it will do nothing. Also, "image" should be a full URL.</li>
         <li>Categories are listed by name and separated by commas. (e.g. Category 1, Category 3)</li>
         <li>Importing CSV files only works with certain browsers. If it is not working on the browser of your choice, try using Firefox, Edge, Chrome or Safari. </li>
+        <li>Importing uses the Product Number and MFG Number as dual identifiers to differentiate between prodcuts.</li>
+        <li>If the product being imported already exists, then the import process will update that product. Otherwise, it will create a new product.</li>
     </ul>
 </p>
 <br />
