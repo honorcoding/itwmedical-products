48d5aeb4bec68a62907c9481ed32da47eeb597bd
honorcoding <john@honorcoding.com>
2026-08-22 09:07:50 -0800

    accessibility adjustments

--- templates/multiple-product/product-filter.php
+++ templates/multiple-product/product-filter.php
@@ -1,39 +1,41 @@
 <div class="itw-product-filter-container">
     <div class="itw-product-filter">
-        <form class="itw-product-filter-form" method="GET">
-        
-            <?php 
-            // itw_category filter
-            if ( ! empty( $options ) ) {
+        <form class="itw-product-filter-form" method="GET" aria-label="Product filter">
 
-                $output = '<div class="itw-form-field">';
+            <fieldset>
+                <legend>Filter products</legend>        
+                <?php 
+                // itw_category filter
+                if ( ! empty( $options ) ) {
 
-                    $output .= '<label for="itw_category">Category</label>';
-                    $output .= '<select id="itw_category" name="itw_category">';
+                    $output = '<div class="itw-form-field">';
 
-                        // select options 
-                        $current = $filter->get_query_var( 'itw_category' );
+                        $output .= '<label for="itw_category">Category</label>';
+                        $output .= '<select id="itw_category" name="itw_category">';
 
-                        foreach( $options as $value => $label ) {
+                            // select options 
+                            $current = $filter->get_query_var( 'itw_category' );
 
-                            $selected = ( $current !== '' && $value == $current ) ? ' selected' : '';
-                            //$link = $filter->add_filter_params_to_url( array( 'itw_category' => $value ) );
+                            foreach( $options as $value => $label ) {
 
-                            $output .= '<option value="' . $value . '"' . $selected . '>';
-                                $output .= $label;
-                            $output .= '</option>';
+                                $selected = ( $current !== '' && $value == $current ) ? ' selected' : '';
+                                //$link = $filter->add_filter_params_to_url( array( 'itw_category' => $value ) );
 
-                        }
+                                $output .= '<option value="' . $value . '"' . $selected . '>';
+                                    $output .= $label;
+                                $output .= '</option>';
 
-                    $output .= '</select>';
-                    $output .= '<input type="submit" value="Filter" />';
+                            }
 
-                $output .= '</div>';
-                echo $output;
+                        $output .= '</select>';
+                        $output .= '<input type="submit" value="Filter" />';
 
-            }
-            ?>
+                    $output .= '</div>';
+                    echo $output;
 
+                }
+                ?>
+            </fieldset>
         </form>
     </div>
 </div>
