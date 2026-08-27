81a5d7f706c2a967a2463f91060c67020778f8d2
honorcoding <john@honorcoding.com>
2026-08-27 13:04:42 -0800

    table adjustments

--- templates/multiple-product/product-list.php
+++ templates/multiple-product/product-list.php
@@ -22,11 +22,11 @@
                     <table class="itw-table">
                         <caption><?php echo $caption; ?></caption>
                         <thead>
-                            <tr class="itw-row itw-header-row">
-                                <th scope="col" class="itw-col">Product Name</th>
-                                <th scope="col" class="itw-col">Product No.</th>
-                                <th scope="col" class="itw-col">Mfg. No.</th>
-                                <th scope="col" class="itw-col">Description</th>
+                            <tr class="itw-header-row">
+                                <th scope="col">Product Name</th>
+                                <th scope="col">Product No.</th>
+                                <th scope="col">Mfg. No.</th>
+                                <th scope="col">Description</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -36,11 +36,11 @@
                                 $link = get_permalink( $product->post_id );                                
                                 ?>
                                     <?php /* <a href="<?php echo $link; ?>" class="itw-row"> */ ?>
-                                    <tr class="itw-row">
-                                        <th scope="row" class="itw-col"><a href="<?php echo $link; ?>" aria-label="<?php echo $product->title . ' - ' . $product->product_number; ?>"><?php echo $product->title; ?></a></th>
-                                        <td class="itw-col"><?php echo $product->product_number; ?></a></td>
-                                        <td class="itw-col"><?php echo $product->mfg_number; ?></a></td>
-                                        <td class="itw-col"><?php echo $product->accessibility_description; ?></a></td>
+                                    <tr>
+                                        <th scope="row"><a href="<?php echo $link; ?>" aria-label="<?php echo $product->title . ' - ' . $product->product_number; ?>"><?php echo $product->title; ?></a></th>
+                                        <td><?php echo $product->product_number; ?></a></td>
+                                        <td><?php echo $product->mfg_number; ?></a></td>
+                                        <td><?php echo $product->accessibility_description; ?></a></td>
                                     </tr>
                                 <?php
 
